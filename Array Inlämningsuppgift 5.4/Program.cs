3e9fb954c104194aa8a29ac289ce7c5264eb926d
Anton <>
2023-03-31 09:04:55 +0200

    Add project files.

---
+++ Array Inlämningsuppgift 5.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] namn = { "Anton", "Kevin", "Theo", "Alfred", "Felix" };
+            Array.Sort(namn);
+
+            Console.WriteLine(namn[0]);
+        }
+    }
+}
